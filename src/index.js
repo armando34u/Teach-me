dcb2e4d21a8a3a64be2a08745cae27f04bfe94f0
Erick <erickxs78@gmail.com>
2019-07-25 11:57:52 -0400

    3er commit-ruta raíz de Login Añadida

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const engine = require('ejs-mate');
+const path = require('path');
+
 
 //Initializations
 const app = express();
 
+
+//settings
+app.set('views', path.join(__dirname,'views'));
+app.engine('ejs',engine);
+app.set('view engine', 'ejs');
+
 app.set('port', process.env.PORT || 3000);
+
+
+//Routes
+app.use('/',require('./routes/index'));
+require('./routes/index');
+
 
 // starting the server
 app.listen(app.get('port'),() =>{
